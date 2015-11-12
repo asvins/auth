cb2f5c3b90a69a5e55fabd978afeff2d6eb10002
Vinicius Correa <viniciuscorrea91@gmail.com>
2015-11-11 23:37:17 -0200

    Added Interceptors\nTODO: resolve IsOwner

--- interceptors.go
+++ interceptors.go
@@ -2,13 +2,72 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 
+	"github.com/asvins/router"
+	"github.com/asvins/router/errors"
 	"github.com/dgrijalva/jwt-go"
 )
 
 type Resource interface {
 	Owner() string
+}
+
+type isAuthorizedInterceptor func(t jwt.Token) bool
+
+func (i isAuthorizedInterceptor) Intercept(w http.ResponseWriter, r *http.Request) errors.Http {
+	tokenString := r.Header.Get("Authorization")
+	if !IsAuthorized(tokenString, i) {
+		return errors.Unauthorized("Unauthorized")
+	}
+	return nil
+}
+
+func IsAuthorizedInterceptor(f func(t jwt.Token) bool) router.Interceptor {
+	return isAuthorizedInterceptor(f)
+}
+
+// type isOwnerInterceptor  Resource
+//
+// func (i isOwnerInterceptor) Intercept(w http.ResponseWriter, r *http.Request) errors.Http {
+// 	tokenString := r.Header.Get("Authorization")
+// 	if !IsOwner(tokenString, i) {
+// 		return errors.Unauthorized("Unauthorized")
+// 	}
+// 	return nil
+// }
+//
+// func IsOwnerInterceptor(r Resource) router.Interceptor {
+// 	return isOwnerInterceptor(r)
+// }
+
+type hasScopeInterceptor string
+
+func (i hasScopeInterceptor) Intercept(w http.ResponseWriter, r *http.Request) errors.Http {
+	tokenString := r.Header.Get("Authorization")
+	if !HasScope(tokenString, string(i)) {
+		return errors.Unauthorized("Unauthorized")
+	}
+	return nil
+}
+
+func HasScopeInterceptor(scope string) router.Interceptor {
+	return hasScopeInterceptor(scope)
+}
+
+type hasOneScopeInterceptor []string
+
+func (i hasOneScopeInterceptor) Intercept(w http.ResponseWriter, r *http.Request) errors.Http {
+	tokenString := r.Header.Get("Authorization")
+	if !HasOneScope(tokenString, []string(i)) {
+		return errors.Unauthorized("Unauthorized")
+	}
+	return nil
+}
+
+func HasOneScopeInterceptor(scopes []string) router.Interceptor {
+	return hasOneScopeInterceptor(scopes)
 }
 
 func IsAuthorized(token string, pred func(t jwt.Token) bool) bool {
