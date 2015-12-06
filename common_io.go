6056328a71a55e332b3cc7418abb857af5534b9b
Rafael Leite <r.cmgleite@gmail.com>
2015-12-05 22:28:10 -0200

    creating models package and fireEvent function for common_io

--- common_io.go
+++ common_io.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
+	"github.com/asvins/auth/models"
 	"github.com/asvins/common_io"
 	"github.com/asvins/utils/config"
 )
+
+const (
+	EVENT_CREATED = iota
+	EVENT_UPDATED
+	EVENT_DELETED
+)
+
+func topic(event int, prefix string) (string, error) {
+	var sufix string
+
+	switch event {
+	case EVENT_CREATED:
+		sufix = "_created"
+	case EVENT_UPDATED:
+		sufix = "_updated"
+	case EVENT_DELETED:
+		sufix = "_deleted"
+	default:
+		return "", errors.New("[ERROR] Event not found")
+	}
+
+	return prefix + sufix, nil
+}
+
+func fireEvent(event int, usr *models.User) {
+	b, err := json.Marshal(usr)
+	if err != nil {
+		// TODO tratar erro...
+		return
+	}
+
+	topic, err := topic(event, "user")
+	if err != nil {
+		// TODO tratar erro...
+		return
+	}
+
+	producer.Publish(topic, b)
+}
 
 func setupCommonIo() {
 	cfg := common_io.Config{}
